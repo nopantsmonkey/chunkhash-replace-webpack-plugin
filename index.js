be0c4c32622e542b04849feb58c953d7b15627ae
Aamir Hussain <aamir.hussain@colwiz.com>
2017-04-04 14:55:00 +0500

    added support for array input, dest now support nested params

--- index.js
+++ index.js
@@ -2,35 +2,52 @@
 
 const path = require('path');
 const fs = require('fs');
+const mkdirp = require('mkdirp');
 const pathSeparator = process.platform.toLowerCase() === 'win32' ? '\\' : '/';
 
 function ChunkHashReplacePlugin(options) {
-    this.src = options.src;
-    this.dest = options.dest;
+    if (options instanceof Array) {
+        this.list = options;
+    } else {
+        this.list = [{
+            src: options.src,
+            dest: options.dest
+        }];
+    }
 }
 
 ChunkHashReplacePlugin.prototype.apply = function(compiler) {
     let self = this;
     let folder = compiler.options.context;
-    let src = path.join(folder, self.src);
-    let dest = path.join(folder, self.dest);
+    self.list.forEach(item => {
+        let psrc = item.src;
+        let pdest = item.dest;
 
-    fs.readFile(src, 'utf8', function(err, data) {
-        compiler.plugin('done', function(statsData) {
-            const stats = statsData.toJson();
-            let htmlOutput = fs.readFileSync(src, 'utf8');
-            for (let chunk of stats.chunks) {
-                const {hash, files} = chunk;
-                files.forEach(file => {
-                    let resourcePath = file.replace('css' + pathSeparator, '').replace(/\\/g, '/');
-                    let resourceURI = resourcePath.replace(`${hash}.`, '');
-                    htmlOutput = htmlOutput.replace(resourceURI, resourcePath);
+        let src = path.join(folder, psrc);
+        let dest = path.join(folder, pdest);
+
+        fs.readFile(src, 'utf8', function(err, data) {
+            compiler.plugin('done', function(statsData) {
+                const stats = statsData.toJson();
+                let htmlOutput = fs.readFileSync(src, 'utf8');
+                for (let chunk of stats.chunks) {
+                    const {hash, files} = chunk;
+                    files.forEach(file => {
+                        let resourcePath = file.replace('css' + pathSeparator, '').replace(/\\/g, '/');
+                        let resourceURI = resourcePath.replace(`${hash}.`, '');
+                        htmlOutput = htmlOutput.replace(resourceURI, resourcePath);
+                    });
+                }
+
+                mkdirp(path.dirname(dest), (err) => {
+                    if(err) {
+                        console.error(err);
+                    }
+                    fs.writeFile(dest, htmlOutput);
                 });
-            }
-
-            fs.writeFile(dest, htmlOutput);
+            });
         });
-    });
+    })
 };
 
 module.exports = ChunkHashReplacePlugin;
